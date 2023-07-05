919d651053a2894d199eeaff2d2718ef89ebeb3c
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-04 19:42:14 -0600

    test DELETE

--- Develop/server.js
+++ Develop/server.js
@@ -27,7 +27,6 @@ app.get('/', (req, res) => {
 });
 // route for notes page
 app.get('/notes', (req, res) => {
-    console.log("Notes");
     return res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
@@ -35,21 +34,25 @@ app.get('/notes', (req, res) => {
 // API
 // fetch json data
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received`);
+    // console.info(`${req.method} request received`);
     return res.status(200).json(notes);
 });
 // post json data
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`);
-    console.info(`${req.method} request received`);
+    // res.json(`${req.method} request received`);
+    // console.info(`${req.method} request received`);
 
     let jsonDataPath = path.join(__dirname, './db/db.json')
 
-    const newNote = req.body;
-    // console.log(note);
+    let newNote = req.body;
 
     notes.push(newNote);
-    // console.log(notes);
+
+    // assign id to notes so they can be tracked for deletion
+    notes.forEach((item, i) => {
+        item.id = i + 1;
+    });
+    
 
     fs.writeFile(jsonDataPath, JSON.stringify(notes), 
     (writeErr) =>
@@ -57,18 +60,43 @@ app.post('/api/notes', (req, res) => {
               ? console.error(writeErr)
               : console.info('Successfully saved note!')
         );
-
+    console.log(notes);
     res.status(200).json(newNote);
 
 });
 
 
 // BONUS delete POST
+app.delete('/api/notes/:id', (req, res) => {
+    let jsonDataPath = path.join(__dirname, './db/db.json');
+    // variable to store value of parameter linked to id number assigned during POST
+    let noteID = req.params.id;
+
+    // assigns array index to id sequence
+    for (let i = 0; i < notes.length; i++) {
+        // if index number matches ID number, takes that index number and splices out of array
+        if (notes[i].id == noteID) {
+            notes.splice(i, 1);
+            break;
+        }
+    }
+
+    // rewrites file with the spliced array
+    fs.writeFileSync(jsonDataPath, JSON.stringify(notes), 
+    (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+    // sends json of new array
+    res.status(200).json(notes);
+    console.log(noteID);
+})
 
 
 // catchall routing back to home
 app.get('*', (req, res) => {
-    console.log("FALLBACK");
+    console.log("URL Not Available");
     return res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
