6095a0eb907f14a625b5e5382df0820e6e5cd559
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-04 20:23:51 -0600

    deploy heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,9 +4,8 @@ const express = require('express');
 const fs = require('fs');
 // import 'path' package
 const path = require('path');
-// notes database
+// import the database file
 const notes = require('./db/db.json');
-
 
 // Express
 // initialize express
@@ -19,7 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 // point to folder with code
 app.use(express.static('public'));
 
-
 // html ROUTES
 // default routing for landing page
 app.get('/', (req, res) => {
@@ -30,73 +28,65 @@ app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-
 // API
 // fetch json data
 app.get('/api/notes', (req, res) => {
-    // console.info(`${req.method} request received`);
     return res.status(200).json(notes);
 });
+
 // post json data
 app.post('/api/notes', (req, res) => {
-    // res.json(`${req.method} request received`);
-    // console.info(`${req.method} request received`);
-
+    // point to path where POST will write new data to database
     let jsonDataPath = path.join(__dirname, './db/db.json')
 
+    // store body contents of json object and push content to array in database 'db.json'
     let newNote = req.body;
-
     notes.push(newNote);
 
-    // assign id to notes so they can be tracked for deletion
-    notes.forEach((item, i) => {
-        item.id = i + 1;
-    });
+    // BONUS assign id to notes so they can be tracked for deletion
+    // id will refresh on POST so later when index is applied, number sequences should always align
+    notes.forEach((item, i) => {item.id = i + 1;});
     
-
+    // overwrite json database file with string including new object from push
     fs.writeFile(jsonDataPath, JSON.stringify(notes), 
     (writeErr) =>
             writeErr
               ? console.error(writeErr)
               : console.info('Successfully saved note!')
         );
-    console.log(notes);
+    // send json response
     res.status(200).json(newNote);
-
 });
 
-
-// BONUS delete POST
+// BONUS delete note
+// include query parameter 'id' to check the route of req.body property 'id' of the "notes" object to be deleted
 app.delete('/api/notes/:id', (req, res) => {
     let jsonDataPath = path.join(__dirname, './db/db.json');
-    // variable to store value of parameter linked to id number assigned during POST
+    // variable to store value of the route property 'id'
     let noteID = req.params.id;
 
-    // assigns array index to id sequence
+    // assigns array index to objects so the index number sequence aligns with 'id' parameter
     for (let i = 0; i < notes.length; i++) {
-        // if index number matches ID number, takes that index number and splices out of array
+        // if index number matches id number, takes object at that index number and splices out of array database
         if (notes[i].id == noteID) {
             notes.splice(i, 1);
             break;
         }
     }
 
-    // rewrites file with the spliced array
+    // rewrites file with the newly spliced array, preserves 'id' and index sequence until new POST event
     fs.writeFileSync(jsonDataPath, JSON.stringify(notes), 
     (writeErr) =>
             writeErr
               ? console.error(writeErr)
-              : console.info('Successfully updated reviews!')
+              : console.info('Successfully updated notes!')
         );
     // sends json of new array
     res.status(200).json(notes);
-    console.log(noteID);
-})
+});
 
-
-// catchall routing back to home
+// catchall routing back to index.html
 app.get('*', (req, res) => {
-    console.log("URL Not Available");
     return res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
